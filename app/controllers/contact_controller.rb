8e6b1066f4117d2a7e6e2c7b650d3096e683c7df
Steve Meisner <steve.meisner@hey.com>
2023-10-31 11:29:20 -0400

    form styles and copy

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,16 +2,10 @@ class ContactController < ApplicationController
   def create
     @contact_form = ContactForm.new(contact_form_params)
 
-    respond_to do |format|
-      if @contact_form.call
-        format.turbo_stream {
-          render turbo_stream: turbo_stream.replace(@contact_form, partial: "contact_form", locals: { contact_form: ContactForm.new, feedback: "success" })
-        }
-        format.html { redirect_to root_path, notice: "Message sent successfully" }
-      else
-        format.turbo_stream { render turbo_stream: turbo_stream.replace(@contact_form, partial: "contact_form", locals: { contact_form: @contact_form }) }
-        format.html { redirect_to root_path, alert: "Message not sent" }
-      end
+    if @contact_form.call
+      @feedback = "success"
+    else
+      @feedback = "invalid"
     end
   end
 
